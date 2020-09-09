ea016a63afe2dd7ab62212c5df2af9dcf66d81eb
github username <souzajohnlee@gmail.com>
2020-09-09 13:08:35 -0700

    basic page setup

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [jsonData, setJsonData] = useState([])
+
+  const populateJsonData = () => {
+    return jsonData.map(data => {
+      return <div>{data}</div>
+    })
+  }
+
+  // useEffect(() => {
+
+  // }, [])
+
   return (
     <div>
       <h1>Covid Stats</h1>
+      {jsonData[0] ? populateJsonData() : "loading..."}
     </div>
   );
 }
