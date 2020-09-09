1ba78ee22375164cb7e6587cef21128630a27637
github username <souzajohnlee@gmail.com>
2020-09-09 13:44:23 -0700

    added hospitalization and icu numbers

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -14,6 +14,12 @@ function App() {
           <li>Total Deaths - {data.death ? data.death : "N/A"}</li>
           <li>Total Deaths (only confirmed) - {data.deathConfirmed ? data.deathConfirmed : "N/A"}</li>
           <li>Currently on Ventilator: - {data.onVentilatorCurrently ? data.onVentilatorCurrently : "N/A"}</li>
+          <li>New Deaths - {data.deathIncrease ? data.deathIncrease : "N/A"}</li>
+          <li>Total People Hospitalized (historical) - {data.hospitalizedCumulative ? data.hospitalizedCumulative : "N/A"}</li>
+          <li>Curently Hospitalized - {data.hospitalizedCurrently ? data.hospitalizedCurrently : "N/A"}</li>
+          <li>New Hospitalizations - {data.hospitalizedIncrease ? data.hospitalizedIncrease : "N/A"}</li>
+          <li>Total ICU (historical) - {data.inIcuCumulative ? data.inIcuCumulative : "N/A"}</li>
+          <li>Currently in ICU - {data.inIcuCurrently ? data.inIcuCurrently : "N/A"}</li>
         </ul>
         </div>
     })
