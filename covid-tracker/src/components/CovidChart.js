d472614f61dd122b8f6e5b348d9ea69238f33265
github username <souzajohnlee@gmail.com>
2020-09-16 13:19:50 -0700

    set up chart file

---
+++ covid-tracker/src/components/CovidChart.js
@@ -0,0 +1,12 @@
+import Chart from 'chart.js';
+import React from 'react';
+
+const CovidChart = () => {
+    return(
+        <div>
+            <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+    )
+};
+
+export default CovidChart;
